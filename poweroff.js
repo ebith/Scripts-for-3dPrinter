2cf3baae0e55b35f9f9435da8e041fef84f1cead
ebith <ebith.h@gmail.com>
2019-08-23 22:17:08 +0900

    Does not shutdown if printing

--- poweroff.js
+++ poweroff.js
@@ -1,15 +1,20 @@
 require('dotenv').config();
-const {post} = require('httpie');
+const {get, post} = require('httpie');
 const tpLink = new (require('tplink-smarthome-api')).Client();
 
 (async () => {
   const plug = tpLink.getPlug({host: process.env.PLUG_HOST});
 
-  const response = await post(`http://${process.env.OCTOPI_HOST}/api/system/commands/core/shutdown`, {
+  const {data} = await get(`http://${process.env.OCTOPI_HOST}/api/printer?exclude=temperature,sd`, {
     headers: {'X-Api-Key': process.env.API_KEY}
   });
+  if (!data.state.flags.printing) {
+    await post(`http://${process.env.OCTOPI_HOST}/api/system/commands/core/shutdown`, {
+      headers: {'X-Api-Key': process.env.API_KEY}
+    });
 
-  setTimeout(async () => {
-    await plug.setPowerState(false);
-  }, 30 * 1000);
+    setTimeout(async () => {
+      await plug.setPowerState(false);
+    }, 30 * 1000);
+  }
 })();
